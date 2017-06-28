b1f45c5976e741ba74d835492ecb10178b892231
jennifer-shin <jenshin@usc.edu>
2017-06-28 14:49:53 -0700

    updated composing

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -1,13 +1,12 @@
 package com.codepath.apps.restclienttemplate.models;
 
-import android.text.format.DateUtils;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.Serializable;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Locale;
 
 /**
@@ -34,20 +33,39 @@ public class Tweet implements Serializable {
     }
 
     public String getRelativeTimeAgo(String rawJsonDate) {
+        String time = "";
         String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
-        SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
-        sf.setLenient(true);
-
-        String relativeDate = "";
+        SimpleDateFormat format = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+        format.setLenient(true);
         try {
-            long dateMillis = sf.parse(rawJsonDate).getTime();
-            relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
-                    System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
-        } catch (ParseException e) {
+            long diff = (System.currentTimeMillis() - format.parse(rawJsonDate).getTime()) / 1000;
+            if (diff < 5)
+                time = "Just now";
+            else if (diff < 60)
+                time = String.format(Locale.ENGLISH, "%ds",diff);
+            else if (diff < 60 * 60)
+                time = String.format(Locale.ENGLISH, "%dm", diff / 60);
+            else if (diff < 60 * 60 * 24)
+                time = String.format(Locale.ENGLISH, "%dh", diff / (60 * 60));
+            else if (diff < 60 * 60 * 24 * 30)
+                time = String.format(Locale.ENGLISH, "%dd", diff / (60 * 60 * 24));
+            else {
+                Calendar now = Calendar.getInstance();
+                Calendar then = Calendar.getInstance();
+                then.setTime(format.parse(rawJsonDate));
+                if (now.get(Calendar.YEAR) == then.get(Calendar.YEAR)) {
+                    time = String.valueOf(then.get(Calendar.DAY_OF_MONTH)) + " "
+                            + then.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.US);
+                } else {
+                    time = String.valueOf(then.get(Calendar.DAY_OF_MONTH)) + " "
+                            + then.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.US)
+                            + " " + String.valueOf(then.get(Calendar.YEAR) - 2000);
+                }
+            }
+        }  catch (ParseException e) {
             e.printStackTrace();
         }
-
-        return relativeDate;
+        return time;
     }
 }
 
